9fc12cf04aafa676675d74cb80a10ae24203ccf6
Ashish Mishra <ashishk861@gmail.com>
2021-01-08 00:31:01 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <Counter></Counter>
+       <Counter by="1"></Counter>
+       <Counter by="5"></Counter>
+       <Counter by="10"></Counter>
       </div>
     );
   }
