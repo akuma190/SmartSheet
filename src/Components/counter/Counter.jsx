a3d8f414faf863d0bd47c84fbf8d004b3f36eac7
Ashish Mishra <ashishk861@gmail.com>
2021-01-08 00:31:04 -0500

    Update Counter.jsx

--- src/Components/counter/Counter.jsx
+++ src/Components/counter/Counter.jsx
@@ -8,22 +8,38 @@ class Counter extends Component {
         super();
         //unles we call the super we cannot use this in the javascript.
         this.state = {
-            counter: 0
+            counter: 0,
+            secondCounter:0
         }
         //if we want to make the counter available to the method tot he class.
         //we will have to bind the method to the class
         //we will have to do that inside the construcor.
-        this.increment=this.increment.bind(this);
+       this.increment=this.increment.bind(this);
+       //we are comneting it as we are now using the arrow function.
     }
 
     render() {
+        //we can directly use the css below 
+        const style={fontSize:"50px",padding:"15px 30px"};
         return (
             <div className="counter">
-                <button onClick={this.increment}>+1</button>
+                <button onClick={this.increment}>{this.props.by}</button>
                 <span className="count">{this.state.counter}</span>
+                <span className="count">{this.state.secondCounter}</span>
             </div>
         );
     }
+   //we will be changing the abpve code to the arrow function.
+    // render=() =>{
+    //     return (
+    //         <div className="counter">
+    //             <button onClick={this.increment}>+1</button>
+    //             <span className="count">{this.state.counter}</span>
+    //             <span className="count">{this.state.secondCounter}</span>
+    //         </div>
+    //     );
+    // }
+
     increment() {//
         //here we are directly changing the state rather than this we should use the useState();
         //this.state.counter++;
@@ -31,6 +47,15 @@ class Counter extends Component {
             state:this.state.counter++
         });
     }
+
+    //we will also change the increment to the arrow method.
+    // increment = () => {//
+    //     //here we are directly changing the state rather than this we should use the useState();
+    //     //this.state.counter++;
+    //     this.setState({
+    //         state:this.state.counter++
+    //     });
+    // }
 }
 
 
